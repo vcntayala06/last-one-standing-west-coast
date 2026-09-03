754c2200771b20c29c43c20a7d19b3f4cd42ea19
vcntayala06 <vcnt.ayala06@gmail.com>
2026-09-03 07:21:40 -0700

    Lock West Coast You're Up master runtime screen

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_VERSION="6.38-7aa10e2f6f9c";
+const CACHE_VERSION="6.38-a1eaf0277665";
 const CACHE_NAME=`last-one-standing-shell-${CACHE_VERSION}`;
 const APP_SHELL=[
  "./",
