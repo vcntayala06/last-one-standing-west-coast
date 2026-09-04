3464c2faa1fa46c75bba5df87caff3f4c6fa6162
vcntayala06 <vcnt.ayala06@gmail.com>
2026-09-03 20:04:05 -0700

    Lock West Coast Question master runtime screen

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_VERSION="6.38-a1eaf0277665";
+const CACHE_VERSION="6.38-342e2ea42bdd";
 const CACHE_NAME=`last-one-standing-shell-${CACHE_VERSION}`;
 const APP_SHELL=[
  "./",
