e272b18b34a25a973d84cc85ce1106c034eece43
vcntayala06 <vcnt.ayala06@gmail.com>
2026-09-13 19:55:31 -0700

    Polish mobile Showtime handoff and timeout

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_VERSION="6.38-iphone-hierarchy";
+const CACHE_VERSION="6.38-mobile-three-screen-polish";
 const CACHE_NAME=`last-one-standing-shell-${CACHE_VERSION}`;
 const APP_SHELL=[
  "./",
