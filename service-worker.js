3a2d08523d10a12dca92c195f899a56cc26ea4fa
vcntayala06 <vcnt.ayala06@gmail.com>
2026-09-13 18:53:43 -0700

    Correct iPhone game screen compositions

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_VERSION="6.38-mobile-voice-nav";
+const CACHE_VERSION="6.38-iphone-screen-fix";
 const CACHE_NAME=`last-one-standing-shell-${CACHE_VERSION}`;
 const APP_SHELL=[
  "./",
