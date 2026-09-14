41199a6f404d0d05c8866ea2c4a485763edd120d
vcntayala06 <vcnt.ayala06@gmail.com>
2026-09-14 06:58:29 -0700

    Balance mobile result spacing

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_VERSION="6.38-mobile-overlap-fix";
+const CACHE_VERSION="6.38-mobile-result-rhythm";
 const CACHE_NAME=`last-one-standing-shell-${CACHE_VERSION}`;
 const APP_SHELL=[
  "./",
