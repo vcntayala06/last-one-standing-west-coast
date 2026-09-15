199c04fa725f5ea69a2b8a87a4f1c81fe1c2a9fe
vcntayala06 <vcnt.ayala06@gmail.com>
2026-09-14 21:15:47 -0700

    Polish mobile landscape result screens

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_VERSION="6.38-mobile-screen-polish";
+const CACHE_VERSION="6.38-mobile-landscape-polish";
 const CACHE_NAME=`last-one-standing-shell-${CACHE_VERSION}`;
 const APP_SHELL=[
  "./",
